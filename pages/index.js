ae5060290326a9d452e4a7e82f734d795f020979
J0SAL <joyalmeida321@gmail.com>
2024-01-20 21:01:30 +0530

    removed unneeded js

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,10 @@
 import React from "react";
-import Script from "next/script";
 import { Container } from "react-bootstrap";
 import Home from "../components/home/Home";
 
 function HomePage() {
   return (
     <>
-      <Script src="/static/scripts.js"></Script>
       <Container>
         <Home />
       </Container>

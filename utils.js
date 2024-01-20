c5a0690a57f77d9174d9b424d15c2a5b6a304a9c
J0SAL <joyalmeida321@gmail.com>
2024-01-20 20:06:01 +0530

    updated utils

--- utils.js
+++ utils.js
@@ -1,4 +1,3 @@
-import Axios from "axios";
 import { toast } from "react-toastify";
 
 export function getUserAvatar(name, size = 128) {
@@ -7,38 +6,12 @@ export function getUserAvatar(name, size = 128) {
   )}&background=random&size=${size}`;
 }
 
-const weekday = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-var today = new Date().getDay();
-export const labels = weekday.map((item, i) => weekday[(i + today) % 7]);
+export const today = new Date().getDay();
 
 export function sentenceCase(str) {
   return str.replace(/\w\S*/g, function (txt) {
     return txt.charAt(0).toUpperCase() + txt.substr(1);
   });
-}
-
-export async function uploadFilesToCloud(image) {
-  const key = process.env.NEXT_PUBLIC_CLOUDINARY_PRESET;
-  const url = process.env.NEXT_PUBLIC_CLOUDINARY_URL;
-  if (image === undefined) return;
-  const formData = new FormData();
-  formData.append("file", image);
-
-  formData.append("upload_preset", key);
-  try {
-    const res = await Axios.post(url, formData);
-    return await res;
-  } catch (err) {
-    console.log(err);
-  }
 }
 
 export const alertBox = (message = "Something went wrong", type = "error") => {
